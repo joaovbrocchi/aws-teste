ec3fc7eacb38d2806885f08e11dd849882c2d621
joaovbrocchi <joaobrocchiprofissional@gmail.com>
2024-06-27 23:26:55 -0300

    yeye

--- back-end/index.js
+++ back-end/index.js
@@ -1,9 +1,8 @@
 import express, { urlencoded } from "express";
 import { Sequelize, DataTypes } from "sequelize";
-import  {counter}  from "./prometheus/Prometheus.js";
+import  {counter,histogram, summary, gauge}  from "./prometheus/Prometheus.js";
 import cors from "cors";
 import dotenv from "dotenv";
-import { Registry } from "prom-client";
 import prom from "prom-client"
 dotenv.config()
 
@@ -12,9 +11,7 @@ const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_US
   host: process.env.MYSQL_HOST,
   dialect: "mysql",
 });
-// Restante do seu código Sequelize e Express
 
-// Definir o modelo Usuario
 const Usuario = sequelize.define('Usuario', {
   nome: {
     type: DataTypes.STRING,
@@ -26,21 +23,19 @@ const Usuario = sequelize.define('Usuario', {
   },
 });
 
-// Sincronizar o modelo com o banco de dados
 sequelize.sync();
-
-// Criar a aplicação Express
 const app = express();
-
-// Middleware para parsear JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Middleware para habilitar CORS
 app.use(cors());
-// Rota GET para listar todos os usuários
+
 app.get('/usuarios', async (req, res) => {
+  const responseTime = Math.random();
   counter.inc();
+  gauge.set(100 * Math.random())
+  histogram.observe(responseTime)
+  summary.observe(responseTime)
+
   try {
     const usuarios = await Usuario.findAll();
     res.json(usuarios);
@@ -60,13 +55,14 @@ app.post('/usuarios', async (req, res) => {
   }
 });
 
+app.get("/metrics", async (req, res)=>{
+  res.set("Content-Type", prom.register.contentType)
+  res.send( await prom.register.metrics());
+})
+
 // Iniciar o servidor
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
 
-app.get("/metrics", async (req, res)=>{
-  res.set("Content-Type", prom.register.contentType)
-  res.end( await prom.register.metrics());
-})
--- back-end/prometheus/Prometheus.js
+++ back-end/prometheus/Prometheus.js
@@ -1,5 +1,23 @@
-import { Counter } from "prom-client";
+import { Counter, Gauge, Histogram, Summary } from "prom-client";
+
+
 export const counter = new Counter({
   name: 'request_total',
   help: 'Contador de Reequest',
+});
+
+export const gauge = new Gauge({
+  name: 'request_gauge',
+  help: 'metric_help',
+});
+
+export const histogram = new Histogram({
+  name: 'request_histogram',
+  help: 'metric_help',
+  buckets: [0.1, 0.2, 0.3, 0.4, 0.5]
+});
+export const summary = new Summary({
+  name: 'request_summary',
+  help: 'metric_help',
+  percentiles: [0.01, 0.1, 0.9, 0.99],
 });
